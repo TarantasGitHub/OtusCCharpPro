a2c536cc062d3d819ca4fee6c5a5f8b4990d25d9
Андрей <an_sviridov@hotmail.com>
2024-02-24 06:58:39 +0300

    Удаление лишнего

--- ClassLibraryContracts/Entities/BaseEntity.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ClassLibraryContracts.Entities
-{
-    public abstract class BaseEntity<TKey> where TKey : notnull
-    {
-        public TKey Id { get; init; }
-    }
-}
--- ClassLibraryContracts/Models/BaseEntityDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ClassLibraryContracts.Models
-{
-    public abstract class BaseEntityDto<TKey> where TKey : notnull
-    {
-        public TKey Id { get; set; }
-    }
-}
